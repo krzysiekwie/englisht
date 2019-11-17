404f0a819c17145154092acd1da200de9a5cb8d8
krzysiekwie@gmail.com <krzysiekwie@gmail.com>
2019-11-17 14:34:17 +0100

    form names before game

--- tic-tac-verb/game.js
+++ tic-tac-verb/game.js
@@ -1,59 +1,57 @@
 //btnReset
-var reset = document.querySelector("#btnReset")
-var btnDo = document.querySelector("#btnDo")
-var btnGo = document.querySelector("#btnGo")
-
+var reset = document.querySelector("#btnReset");
+var btnDo = document.querySelector("#btnDo");
+var btnGo = document.querySelector("#btnGo");
 
 //all boxes
-var boxes = document.querySelectorAll("td")
+var boxes = document.querySelectorAll("td");
 
 //clear boxes
 function clrBoxes() {
     for (var i = 0; i < boxes.length; i++) {
-      boxes[i].textContent = "";
-      boxes[i].style.background = 'none';
+        boxes[i].textContent = "";
+        boxes[i].style.background = "none";
     }
-    verbs = ['1','2','3']
+    verbs = ["present", "past", "3rd form"];
 }
 
-reset.addEventListener('click', clrBoxes);
+reset.addEventListener("click", clrBoxes);
 
 //change verbs
-var verbs = ['1', '2', '3']
-var color = ''
+var verbs = ["present", "past", "3rd form"];
+var color = "";
 
 function verbDo() {
-  verbs = ['do', 'did', 'done'];
-  color = 'lightgreen';
+    verbs = ["do", "did", "done"];
+    color = "lightgreen";
 }
 
 function verbGo() {
-  verbs = ['go', 'went', 'gone'];
-  color = 'lightblue';
+    verbs = ["go", "went", "gone"];
+    color = "lightblue";
 }
 
-btnDo.addEventListener('click',verbDo);
-btnGo.addEventListener('click',verbGo);
-
+btnDo.addEventListener("click", verbDo);
+btnGo.addEventListener("click", verbGo);
 
 // change box value
 
-function changeBoxes(){
-  if (this.textContent === '') {
-    this.textContent = verbs[0];
-    this.style.background = color;
-  } else if (this.textContent === verbs[0]) {
-    this.textContent = verbs[1];
-    this.style.background = color;
-  } else if (this.textContent === verbs[1]) {
-    this.textContent = verbs[2];
-    this.style.background = color;
-  } else {
-    this.textContent = '';
-    this.style.background = 'none';
-  }
+function changeBoxes() {
+    if (this.textContent === "") {
+        this.textContent = verbs[0];
+        this.style.background = color;
+    } else if (this.textContent === verbs[0]) {
+        this.textContent = verbs[1];
+        this.style.background = color;
+    } else if (this.textContent === verbs[1]) {
+        this.textContent = verbs[2];
+        this.style.background = color;
+    } else {
+        this.textContent = "";
+        this.style.background = "none";
+    }
 }
 
 for (var i = 0; i < boxes.length; i++) {
-  boxes[i].addEventListener('click', changeBoxes)
+    boxes[i].addEventListener("click", changeBoxes);
 }
